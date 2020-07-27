6fa496ed90adee87ccb22987393ff6df6484458f
Jose Terrera <joseterrera@yahoo.com>
2020-07-26 22:05:13 -0700

    redoing table doctors

--- doctors.sql
+++ doctors.sql
@@ -7,28 +7,50 @@ CREATE DATABASE doctors;
 
 \c doctors
 
-CREATE TABLE doctors
-(
-  id SERIAL PRIMARY KEY,
-  name TEXT NOT NULL,
-  specialty TEXT NOT NULL,
-  location TEXT NOT NULL,
-  patients_this_doctor_has INTEGER REFERENCES patient ON DELETE CASCADE
-);
 
+CREATE TABLE doctors (
+  id SERIAL PRIMARY KEY
+  name TEXT NOT NULL,
+  specialty TEXT NOT NULL
+
+)
 
 CREATE TABLE patients (
-  patient_id SERIAL PRIMARY KEY,
-  patient_name TEXT NOT NULL,
-  insurance TEXT NOT NULL,
-  birthday TEXT NOT NULL,
-);
-
-
-CREATE TABLE diseases (
-  id
-  name
-  -- Many patients may have many diseases
-  patient_id INTEGER REFERENCES patient ON DELETE CASCADE
+  id SERIAL PRIMARY KEY
+  insurance TEXT NOT NULL
+  birthday TEXT not NULL
 )
+
+
+CREATE TABLE visits (
+  id SERIAL PRIMARY KEY
+  doctor_id INTEGER REFERENCES doctors
+  patient_id INTEGER REFERENCES patients
+)
+
+
+-- CREATE TABLE doctors
+-- (
+--   id SERIAL PRIMARY KEY,
+--   name TEXT NOT NULL,
+--   specialty TEXT NOT NULL,
+--   location TEXT NOT NULL,
+--   patients_this_doctor_has INTEGER REFERENCES patient ON DELETE CASCADE
+-- );
+
+
+-- CREATE TABLE patients (
+--   patient_id SERIAL PRIMARY KEY,
+--   patient_name TEXT NOT NULL,
+--   insurance TEXT NOT NULL,
+--   birthday TEXT NOT NULL,
+-- );
+
+
+-- CREATE TABLE diseases (
+--   id
+--   name
+--   -- Many patients may have many diseases
+--   patient_id INTEGER REFERENCES patient ON DELETE CASCADE
+-- )
 
