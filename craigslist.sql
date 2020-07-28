b9a3228b9d3a0cd80e0e02f09612ba0a01f3c23b
Jose Terrera <joseterrera@yahoo.com>
2020-07-27 18:45:23 -0700

    moving forward with categories

--- craigslist.sql
+++ craigslist.sql
@@ -1,23 +1,39 @@
-POSTS
-id
-title
-text
-location
-user_id
-region_id
-category_id
+-- from the terminal run:
+-- psql < doctors.sql
 
-USERS
-username
-password
-preferred_region_id
+DROP DATABASE IF EXISTS craigslist;
+
+CREATE DATABASE craigslist;
+
+\c craigslist
 
 
-REGION
-id
-name
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username TEXT NOT NULL,
+  password TEXT NOT NULL,
+  users_location TEXT REFERENCES region
+)
+
+CREATE TABLE region (
+  id SERIAL PRIMARY KEY
+  location TEXT NOT NULL
+)
+
+CREATE TABLE categories (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+)
+
+CREATE TABLE posts (
+  id SERIAL PRIMARY KEY,
+  title TEXT NOT NULL,
+  description TEXT NOT NULL,
+  user_id INTEGER REFERENCES users,
+  category_id INTEGER REFERENCES category,
+  location_id INTEGER REFERENCES region
+
+)
 
 
-CATEGORY
-id
-name
