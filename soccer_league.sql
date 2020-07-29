2813387fa5c77a650e493e0c955a5b79cda5880e
Jose Terrera <joseterrera@yahoo.com>
2020-07-29 10:48:53 -0700

    creating table

--- soccer_league.sql
+++ soccer_league.sql
@@ -22,15 +22,49 @@ CREATE TABLE players (
 );
 
 CREATE TABLE referees (
-  
+   id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+CREATE TABLE season (
+  id SERIAL PRIMARY KEY,
+  start_date TEXT not NULL,
+  end_date TEXT NOT null
+);
+
+CREATE TABLE matches (
+  id SERIAL PRIMARY key,
+  location TEXT NOT NULL,
+  date TEXT NOT NULL,
+  start_time TEXT NOT NULL,
+  season_id INTEGER REFERENCES season,
+  home_team_id INTEGER REFERENCES teams,
+  away_team_id INTEGER REFERENCES teams,
+  head_referee_if INTEGER REFERENCES referees.
+  assistant_referee_1_id INTEGER REFERENCES referees,
+  assistant_referee_2_id INTEGER REFERENCES referees
+);
+
+CREATE TABLE lineups (
+    id SERIAL PRIMARY KEY,
+    player_id INTEGER REFERENCES players,
+    match_id INTEGER REFERENCES matches,
+    team_id INTEGER REFERENCES teams
+);
+
+
+
+
+CREATE TABLE goals(
+  id SERIAL PRIMARY KEY,
+  player_id INTEGER REFERENCES players,
+  match_id INTEGER REFERENCES matches
+
+);
+
+-- can we make a table with 3 options?
+CREATE TABLE results (
+  id SERIAL PRIMARY KEY,
+  team_id INTEGER REFERENCES teams,
+  match_id INTEGER REFERENCES matches
 )
-
-CREATE TABLE lineups ()
-
-CREATE TABLE season ()
-å
-CREATE TABLE matches ()
-
-CREATE TABLE goals()
-
-CREATE TABLE results ()
