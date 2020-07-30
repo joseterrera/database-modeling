f7e093ac5f6b548cdf25343072ee180a5681701c
Jose Terrera <joseterrera@yahoo.com>
2020-07-29 18:30:34 -0700

    deleted ids on insert  on soccer league as not really needed

--- soccer_league.sql
+++ soccer_league.sql
@@ -69,56 +69,56 @@ CREATE TABLE results (
 
 
 INSERT INTO teams
-  (id, name, city)
+  (name, city)
 VALUES
-  (1, 'Team Portland', 'Oregon'),
-  (2, 'Team Beaverton', 'Oregon'),
-  (3, 'Team Ocreanside', 'California');
+  ( 'Team Portland', 'Oregon'),
+  ( 'Team Beaverton', 'Oregon'),
+  ( 'Team Ocreanside', 'California');
 
 -- any other way to eacape quotes
 INSERT INTO players
-  (id, name, birthday, height, team_id)
+  (name, birthday, height, team_id)
 VALUES
-  (1, 'Ricky', 'Sep 11, 1999', '5 11', 1),
-  (1, 'Joe', 'Aug 1, 1991', '5 6', 2),
-  (1, 'Jerry', 'Oct 18, 1995', '5 9', 3);
+  ( 'Ricky', 'Sep 11, 1999', '5 11', 1),
+  ( 'Joe', 'Aug 1, 1991', '5 6', 2),
+  ( 'Jerry', 'Oct 18, 1995', '5 9', 3);
 
 
 INSERT INTO referees
-  (id, name)
+  (name)
 VALUES
-  (1, 'Jones'),
-  (2, 'Peters'),
-  (3, 'Murphy'),
-  (4, 'Sanchez');
+  ('Jones'),
+  ('Peters'),
+  ('Murphy'),
+  ('Sanchez');
 
 
 INSERT INTO season
-  (id, start_date, end_date)
+  (start_date, end_date)
 VALUES
-  (1, 'March 20 2020', 'June 21, 2020'),
-  (2, 'July 11, 2020', 'Aug 20, 2020'),
-  (3, 'September 10, 2020', 'October 30, 2020');
+  ('March 20 2020', 'June 21, 2020'),
+  ('July 11, 2020', 'Aug 20, 2020'),
+  ('September 10, 2020', 'October 30, 2020');
 
 
 INSERT into matches
-  (id, location, date, start_time, season_id, home_team_id, away_team_id, head_referee_id,assistant_referee_1_id , assistant_referee_2_id)
+  (location, date, start_time, season_id, home_team_id, away_team_id, head_referee_id,assistant_referee_1_id , assistant_referee_2_id)
 VALUES
-  (1, 'OR', 'June 12, 2020', '3om', '1', '2', '3', '1', '2', '3'),
-  (1, 'OR', 'June 19, 2020', '1om', '2', '2', '1', '3', '2', '1'),
-  (1, 'CA', 'September 10, 2020', '12om', '3', '1', '2', '2', '3', '3'),
-  (1, 'OR', 'August 1, 2020', '3om', '1', '3', '2', '1', '2', '3');
+  ('OR', 'June 12, 2020', '3om', '1', '2', '3', '1', '2', '3'),
+  ('OR', 'June 19, 2020', '1om', '2', '2', '1', '3', '2', '1'),
+  ('CA', 'September 10, 2020', '12om', '3', '1', '2', '2', '3', '3'),
+  ('OR', 'August 1, 2020', '3om', '1', '3', '2', '1', '2', '3');
 
 
 
-INSERT into table lineups
-    (id, player_id, match_id, team_id)
+INSERT INTO lineups
+    (player_id, match_id, team_id)
 VALUES
-  (1,2,3,1),
-  (2,3,2,1),
-  (3,3,1,2);
+  (2,3,1),
+  (3,2,1),
+  (3,1,2);
 
-INSERT INTO TABLE goals
+INSERT INTO goals
   (id, player_id, match_id)
 VALUES
   (1, 2, 2),
@@ -126,9 +126,14 @@ VALUES
   (3, 2, 2);
 
 
-INSERT INTO TABLE results
+INSERT INTO results
   (id, team_id, match_id)
 VALUES
   (1, 2, 3),
   (2, 1, 1),
   (3, 2, 2);
+
+
+  -- what would be a good day to change the dates so you can run a query that searches between dates
+
+  
